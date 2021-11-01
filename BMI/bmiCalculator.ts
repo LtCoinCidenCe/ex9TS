cd5575aa139bad69b5d98dba7e2245213ad86d3d
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-11-01 16:51:23 +0200

    9.5

--- BMI/bmiCalculator.ts
+++ BMI/bmiCalculator.ts
@@ -1,17 +1,4 @@
-const calculateBmi = (height: number, weight: number): string => {
-  // height in centimeters, weight in kilograms
-  const h = height / 100;
-  const bmi = weight / h / h;
-  // console.log('bmi:', bmi);
-  if (bmi < 16) return 'Underweight (Severe thinness)';
-  if (bmi >= 16 && bmi < 17) return 'Underweight (Moderate thinness)';
-  if (bmi >= 17 && bmi < 18.5) return 'Underweight (Mild thinness)';
-  if (bmi >= 18.5 && bmi < 25) return 'Normal (healthy weight)';
-  if (bmi >= 25 && bmi < 30) return 'Overweight (Pre-obese)';
-  if (bmi >= 30 && bmi < 35) return 'Obese (Class I)';
-  if (bmi >= 35 && bmi < 40) return 'Obese (Class II)';
-  if (bmi >= 40) return 'Obese (Class III)';
-}
+import { calculateBmi } from './myModule'
 
 interface BodyValues {
   value1: number;
@@ -40,7 +27,7 @@ try {
 catch (error: unknown) {
   let errorMessage = '';
   if (error instanceof Error) {
-    errorMessage = ' Error: ' + error.message;
+    errorMessage = 'Error: ' + error.message;
   }
   console.log(errorMessage);
 }
--- BMI/index.ts
+++ BMI/index.ts
@@ -1,8 +1,29 @@
 import express from 'express';
+import { calculateBmi } from './myModule';
+
 const app=express();
 
 app.get('/hello', (_request, response) => {
   return response.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (request, response) =>
+{
+  const weight = Number(request.query.weight);
+  const height = Number(request.query.height);
+  if (!isNaN(weight) && !isNaN(height))
+  {
+    const words = calculateBmi(height, weight);
+    return response.json({
+      weight: weight,
+      height: height,
+      bmi: words
+    })
+  }
+  else
+    return response.status(401).json({
+      error: "malformatted parameters"
+    });
 });
 
 const PORT = 3003;
