5e9efb29e363e5ce8637b517338d3283d74961f7
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-11-05 17:19:56 +0200

    9.15

--- typedcourse/src/App.tsx
+++ typedcourse/src/App.tsx
@@ -1,21 +1,91 @@
 import React from 'react';
 
-interface course {
+// new types
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
 }
+
+interface CoursePartWithDesc extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartWithDesc {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartWithDesc {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartWithDesc {
+  type: "special";
+  requirements: string[];
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+
+
+
+
+
+
+
+
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
 
 const Header = ({ courseName }: { courseName: string }) => {
   return <h1>{courseName}</h1>
 };
 
-const Content = ({ courseParts }: { courseParts: Array<course>}) => {
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.type) {
+    case "normal":
+      return <div style={{ margin: "1em 0" }}>
+        <div style={{ fontWeight: "bold" }}>{part.name} {part.exerciseCount}</div>
+        <div style={{ fontStyle: "italic" }}>{part.description}</div>
+      </div>
+    case "groupProject":
+      return <div style={{ margin: "1em 0" }}>
+        <div style={{ fontWeight: "bold" }}>{part.name} {part.exerciseCount}</div>
+        <div>project exercises {part.groupProjectCount}</div>
+      </div>
+    case "submission":
+      return <div style={{ margin: "1em 0" }}>
+        <div style={{ fontWeight: "bold" }}>{part.name} {part.exerciseCount}</div>
+        <div style={{ fontStyle: "italic" }}>{part.description}</div>
+        <div>submit to {part.exerciseSubmissionLink}</div>
+      </div>
+    case "special":
+      return <div style={{ margin: "1em 0" }}>
+        <div style={{ fontWeight: "bold" }}>{part.name} {part.exerciseCount}</div>
+        <div style={{ fontStyle: "italic" }}>{part.description}</div>
+        <div>required skills: {part.requirements.join(',')}</div>
+      </div>
+    default:
+      return assertNever(part);
+  }
+}
+
+const Content = ({ courseParts }: { courseParts: Array<CoursePart>}) => {
   return <div>
-    {courseParts.map((part) => <p key={part.name}>{part.name} {part.exerciseCount}</p>)}
+    {courseParts.map((part) => <Part key={part.name} part={part} />)}
   </div>
 }
 
-const Total = ({ courseParts }: { courseParts: Array<course>}) => {
+const Total = ({ courseParts }: { courseParts: Array<CoursePart>}) => {
   return <p>
     Number of exercises {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
   </p>
@@ -23,20 +93,40 @@ const Total = ({ courseParts }: { courseParts: Array<course>}) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
   return (
     <div>
